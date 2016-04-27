a05a550c7b1140bc6b40caf23fd233ab65c13ac1
Simba <dbartell@radford.edu>
2016-04-26 21:02:23 -0400

    Updated UI and Controller

--- KittenSoup/KittenSoup/src/com/spider/kittensoup/Controller.java
+++ KittenSoup/KittenSoup/src/com/spider/kittensoup/Controller.java
@@ -1,12 +1,19 @@
 package com.spider.kittensoup;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
 
 public class Controller 
 {
 	private Button linkButton,mediaButton;
 	private TextField htmlTextField;
+	private Hyperlink gitLink;
 	/*
 	 * Makes links show up in the htmlTextField
 	 */
@@ -27,5 +34,12 @@ public class Controller
 	public void pullHTML()
 	{
 		System.out.println("HTML");
+	}
+	public void gotoGitHub() throws IOException, URISyntaxException
+	{
+		if(Desktop.isDesktopSupported())
+		{
+		  Desktop.getDesktop().browse(new URI("https://github.com/DBartellS/KittenSoup.git"));
+		}
 	}
 }
