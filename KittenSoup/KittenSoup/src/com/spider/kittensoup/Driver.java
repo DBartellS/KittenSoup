fc40ad0ad7f6879f01f1bfca7d8ab97a93dd213f
Simba <dbartell@radford.edu>
2016-04-26 20:26:50 -0400

    Implementation of JavaFX, partial implementation of action listening

--- KittenSoup/KittenSoup/src/com/spider/kittensoup/Driver.java
+++ KittenSoup/KittenSoup/src/com/spider/kittensoup/Driver.java
@@ -1,8 +1,10 @@
 package com.spider.kittensoup;
 
 import java.io.IOException;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 /*
  * @author Daniye S. Bartell
@@ -10,14 +12,21 @@ import javafx.stage.Stage;
  */
 public class Driver extends Application
 {
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		try {
+			AnchorPane root =(AnchorPane) FXMLLoader.load(Driver.class.getResource("SoupGUI.fxml"));
+			Scene scene = new Scene(root,640,480);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) throws IOException
 	{
 		launch(args);
-	}
-
-	@Override
-	public void start(Stage arg0) throws Exception 
-	{
-		
 	}
 }
